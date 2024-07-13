3e397955e0ef5f055582b1a462f037dd71026da7
Sabarno Chatterjee <sabarno.05@gmail.com>
2024-07-13 12:07:35 +0530

    optimized algo

--- Sorting-Algorithms/bubble_sort.cpp
+++ Sorting-Algorithms/bubble_sort.cpp
@@ -2,28 +2,31 @@
 
 using namespace std;
 
-void bubble_sort(int arr[],int n){
+void swap(int *x,int *y){
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+void bubble_sort(int arr[], int n){
+    int flag;
     for(int i=n-1;i>=0;i--){
+        flag=0;
         for(int j=0;j<=i-1;j++){
             if(arr[j]>arr[j+1]){
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+                swap(&arr[j],&arr[j+1]);
+                flag=1;
             }
         }
+        if(flag==0) break;
     }
 }
 
 int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++) cin>>arr[i];
+    int arr[6]={13,46,24,52,20,9};
+    int n=sizeof(arr)/sizeof(arr[0]);
     
-    //sorting algorithm
     bubble_sort(arr,n);
-    
-    //print
     for(int i=0;i<n;i++) cout<<arr[i]<<" ";
     
     return 0;
