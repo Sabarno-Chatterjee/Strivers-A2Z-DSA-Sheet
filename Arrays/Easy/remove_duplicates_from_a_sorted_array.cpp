72bec8f7d9183c63426cd51e1719fce6aa7e8ed8
Sabarno Chatterjee <sabarno.05@gmail.com>
2024-07-16 10:46:10 +0530

    edit

--- Arrays/Easy/remove_duplicates_from_a_sorted_array.cpp
+++ Arrays/Easy/remove_duplicates_from_a_sorted_array.cpp
@@ -1,4 +1,5 @@
 //Brute Force approach
+
 //We can iterate through an array and inserted arr[i] into a set, as set only accepts unique values, we will not get any duplicates, after this we iterate through the set and insert each values at index=0 onwards. The point where our index ends will be the total number of unique elements in the array.
 //Time Complexity: NlogN + N ; for traversing the array and inserting in the set and then for inserting unique elements in the array
 //Space Complexity: O(N); in case the array had all unique elements, we need N places in the set.
@@ -32,6 +33,7 @@
 
 
 // Optimal Approach
+
 // We use a two pointer approach for this.
 // We take the first element as i pointer and second as j pointer, now we keep comparing the j eleement with i, the moment we get an unique number, we place the j element right after the i element and then increment the i pointer. Eventually we get all the unique elements in the beggining of the array and the number of unique elements is i+1.
 // Time Complexity: O(N)
